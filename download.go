f2362c3606d38aa2e6aa01ad3dfd7ecd5c7a23af
Simon Legner <Simon.Legner@gmail.com>
2018-01-31 22:48:26 +0100

    Deal with existing Git repositories

--- download.go
+++ download.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"path"
+	"path/filepath"
 
 	rpc "github.com/mikkeloscar/aur"
 )
@@ -44,7 +44,7 @@ func getPkgbuildfromABS(pkgN string, dir string) (err error) {
 				return fmt.Errorf("Not in standard repositories")
 			}
 			fmt.Println(boldGreenFg(arrow), boldYellowFg(pkgN), boldGreenFg("found in ABS."))
-			errD := exec.Command("git", "clone", "--single-branch", "--branch", "packages/"+pkg.Name(), url, path.Join(dir, pkg.Name())).Run()
+			errD := gitCheckout(url, "packages/"+pkg.Name(), filepath.Join(dir, pkg.Name()))
 			return errD
 		}
 	}
@@ -64,10 +64,44 @@ func getPkgbuildfromAUR(pkgN string, dir string) (err error) {
 
 	fmt.Println(boldGreenFg(arrow), boldYellowFg(pkgN), boldGreenFg("found in AUR."))
 	url := "aur@aur.archlinux.org/" + aq[0].Name + ".git"
-	err = exec.Command("git", "clone", "ssh+git://"+url, path.Join(dir, aq[0].Name)).Run()
+	err = gitCheckout("ssh+git://"+url, "master", filepath.Join(dir, aq[0].Name))
 	if err != nil {
 		// attempt https protocol which does not require SSH key setup
-		err = exec.Command("git", "clone", "https://"+url, path.Join(dir, aq[0].Name)).Run()
+		err = gitCheckout("https://"+url, "master", filepath.Join(dir, aq[0].Name))
 	}
 	return
+}
+
+func gitCheckout(repo, branch, path string) error {
+	// based on https://github.com/golang/tools/blob/master/cmd/tip/tip.go checkout
+	// Clone git repo if it does not exist.
+	if _, err := os.Stat(filepath.Join(path, ".git")); os.IsNotExist(err) {
+		if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
+			return fmt.Errorf("mkdir: %v", err)
+		}
+		if err := exec.Command("git", "clone", "--single-branch", "--branch", branch, repo, path).Run(); err != nil {
+			return fmt.Errorf("clone: %v", err)
+		}
+		return nil
+	} else if err != nil {
+		return fmt.Errorf("stat .git: %v", err)
+	}
+
+	// Pull down changes and update to hash.
+	cmd := exec.Command("git", "fetch")
+	cmd.Dir = path
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("fetch: %v", err)
+	}
+	cmd = exec.Command("git", "reset", "--hard", "origin/"+branch)
+	cmd.Dir = path
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("reset: %v", err)
+	}
+	cmd = exec.Command("git", "clean", "-d", "-f", "-x")
+	cmd.Dir = path
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("clean: %v", err)
+	}
+	return nil
 }
