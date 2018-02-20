85a99198c38a49ebdf5b5675baa0421d7d542f22
morganamilo <morganamilo@gmail.com>
2018-02-20 17:03:50 +0000

    Fix handling of -- in parser

--- cmd.go
+++ cmd.go
@@ -699,6 +699,8 @@ func passToPacman(args *arguments) error {
 		argArr = append(argArr, "--noconfirm")
 	}
 
+	argArr = append(argArr, "--")
+
 	argArr = append(argArr, args.formatTargets()...)
 
 	cmd = exec.Command(argArr[0], argArr[1:]...)
