Jguer/yay

commit 5286f3858c9825a7dcb1d61f6bab80ed84864e52

References #109. Begins to add escape codes and string constants

print.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
+	alpm "github.com/jguer/go-alpm"
 	rpc "github.com/mikkeloscar/aur"
 )
+
+const warning = "\x1b[33mWarning:\x1b[0m "
+const arrow = "==>"
 
 // Human returns results in Human readable format.
 func human(size int64) string {
@@ -29,25 +34,28 @@ func (q aurQuery) printSearch(start int) {
 		var toprint string
 		if config.SearchMode == NumberMenu {
 			if config.SortMode == BottomUp {
-				toprint += fmt.Sprintf("\x1b[33m%d\x1b[0m ", len(q)+start-i-1)
+				toprint += yellowFg(strconv.Itoa(len(q)+start-i-1) + " ")
 			} else {
-				toprint += fmt.Sprintf("\x1b[33m%d\x1b[0m ", start+i)
+				toprint += yellowFg(strconv.Itoa(start+i) + " ")
 			}
 		} else if config.SearchMode == Minimal {
 			fmt.Println(res.Name)
 			continue
 		}
-		toprint += fmt.Sprintf("\x1b[1m%s/\x1b[33m%s \x1b[36m%s \x1b[0m(%d) ", "aur", res.Name, res.Version, res.NumVotes)
+		toprint += boldWhiteFg("aur/") + boldYellowFg(res.Name) +
+			" " + boldCyanFg(res.Version) +
+			" (" + strconv.Itoa(res.NumVotes) + ") "
+
 		if res.Maintainer == "" {
-			toprint += fmt.Sprintf("\x1b[31;40m(Orphaned)\x1b[0m ")
+			toprint += redFgBlackBg("(Orphaned)") + " "
 		}
 
 		if res.OutOfDate != 0 {
-			toprint += fmt.Sprintf("\x1b[31;40m(Out-of-date)\x1b[0m ")
+			toprint += redFgBlackBg("(Out-of-date)") + " "
 		}
 
 		if _, err := localDb.PkgByName(res.Name); err == nil {
-			toprint += fmt.Sprintf("\x1b[32;40mInstalled\x1b[0m")
+			toprint += greenFgBlackBg("Installed")
 		}
 		toprint += "\n    " + res.Description
 		fmt.Println(toprint)
@@ -60,16 +68,16 @@ func (s repoQuery) printSearch() {
 		var toprint string
 		if config.SearchMode == NumberMenu {
 			if config.SortMode == BottomUp {
-				toprint += fmt.Sprintf("\x1b[33m%d\x1b[0m ", len(s)-i)
+				toprint += yellowFg(strconv.Itoa(len(s)-i) + " ")
 			} else {
-				toprint += fmt.Sprintf("\x1b[33m%d\x1b[0m ", i+1)
+				toprint += yellowFg(strconv.Itoa(i+1) + " ")
 			}
 		} else if config.SearchMode == Minimal {
 			fmt.Println(res.Name())
 			continue
 		}
-		toprint += fmt.Sprintf("\x1b[1m%s/\x1b[33m%s \x1b[36m%s \x1b[0m",
-			res.DB().Name(), res.Name(), res.Version())
+		toprint += boldWhiteFg(res.DB().Name()+"/") + boldYellowFg(res.Name()) +
+			" " + boldCyanFg(res.Version()) + " "
 
 		if len(res.Groups().Slice()) != 0 {
 			toprint += fmt.Sprint(res.Groups().Slice(), " ")
@@ -78,7 +86,7 @@ func (s repoQuery) printSearch() {
 		localDb, err := alpmHandle.LocalDb()
 		if err == nil {
 			if _, err = localDb.PkgByName(res.Name()); err == nil {
-				toprint += fmt.Sprintf("\x1b[32;40mInstalled\x1b[0m")
+				toprint += greenFgBlackBg("Installed")
 			}
 		}
 
@@ -89,17 +97,17 @@ func (s repoQuery) printSearch() {
 
 func printDeps(repoDeps []string, aurDeps []string) {
 	if len(repoDeps) != 0 {
-		fmt.Print("\x1b[1;32m==> Repository dependencies: \x1b[0m")
+		fmt.Print(boldGreenFg(arrow + " Repository dependencies: "))
 		for _, repoD := range repoDeps {
-			fmt.Print("\x1b[33m", repoD, " \x1b[0m")
+			fmt.Print(yellowFg(repoD) + " ")
 		}
 		fmt.Print("\n")
 
 	}
 	if len(aurDeps) != 0 {
-		fmt.Print("\x1b[1;32m==> AUR dependencies: \x1b[0m")
+		fmt.Print(boldGreenFg(arrow + " AUR dependencies: "))
 		for _, aurD := range aurDeps {
-			fmt.Print("\x1b[33m", aurD, " \x1b[0m")
+			fmt.Print(yellowFg(aurD) + " ")
 		}
 		fmt.Print("\n")
 	}
@@ -107,59 +115,21 @@ func printDeps(repoDeps []string, aurDeps []string) {
 
 // PrintInfo prints package info like pacman -Si.
 func PrintInfo(a *rpc.Pkg) {
-	fmt.Println("\x1b[1;37mRepository      :\x1b[0m", "aur")
-	fmt.Println("\x1b[1;37mName            :\x1b[0m", a.Name)
-	fmt.Println("\x1b[1;37mVersion         :\x1b[0m", a.Version)
-	fmt.Println("\x1b[1;37mDescription     :\x1b[0m", a.Description)
-	if a.URL != "" {
-		fmt.Println("\x1b[1;37mURL             :\x1b[0m", a.URL)
-	} else {
-		fmt.Println("\x1b[1;37mURL             :\x1b[0m", "None")
-	}
-	fmt.Println("\x1b[1;37mLicenses        :\x1b[0m", strings.Join(a.License, "  "))
-
-	// if len(a.Provides) != 0 {
-	// 	fmt.Println("\x1b[1;37mProvides        :\x1b[0m",
-	// 	Strings.join(a.Provides, "  "))
-	// } else {
-	// 	fmt.Println("\x1b[1;37mProvides        :\x1b[0m", "None")
-	// }
-
-	if len(a.Depends) != 0 {
-		fmt.Println("\x1b[1;37mDepends On      :\x1b[0m", strings.Join(a.Depends, "  "))
-	} else {
-		fmt.Println("\x1b[1;37mDepends On      :\x1b[0m", "None")
-	}
-
-	if len(a.MakeDepends) != 0 {
-		fmt.Println("\x1b[1;37mMake depends On :\x1b[0m", strings.Join(a.MakeDepends, "  "))
-	} else {
-		fmt.Println("\x1b[1;37mMake depends On :\x1b[0m", "None")
-	}
-
-	if len(a.OptDepends) != 0 {
-		fmt.Println("\x1b[1;37mOptional Deps   :\x1b[0m", strings.Join(a.OptDepends, "  "))
-	} else {
-		fmt.Println("\x1b[1;37mOptional Deps   :\x1b[0m", "None")
-	}
-
-	if len(a.Conflicts) != 0 {
-		fmt.Println("\x1b[1;37mConflicts With  :\x1b[0m", strings.Join(a.Conflicts, "  "))
-	} else {
-		fmt.Println("\x1b[1;37mConflicts With  :\x1b[0m", "None")
-	}
-
-	if a.Maintainer != "" {
-		fmt.Println("\x1b[1;37mMaintainer      :\x1b[0m", a.Maintainer)
-	} else {
-		fmt.Println("\x1b[1;37mMaintainer      :\x1b[0m", "None")
-	}
-
-	fmt.Println("\x1b[1;37mVotes           :\x1b[0m", a.NumVotes)
-	fmt.Println("\x1b[1;37mPopularity      :\x1b[0m", a.Popularity)
-
+	fmt.Println(boldWhiteFg("Repository      :"), "aur")
+	fmt.Println(boldWhiteFg("Name            :"), a.Name)
+	fmt.Println(boldWhiteFg("Version         :"), a.Version)
+	fmt.Println(boldWhiteFg("Description     :"), a.Description)
+	fmt.Println(boldWhiteFg("URL             :"), a.URL)
+	fmt.Println(boldWhiteFg("Licenses        :"), strings.Join(a.License, "  "))
+	fmt.Println(boldWhiteFg("Depends On      :"), strings.Join(a.Depends, "  "))
+	fmt.Println(boldWhiteFg("Make Deps       :"), strings.Join(a.MakeDepends, "  "))
+	fmt.Println(boldWhiteFg("Optional Deps   :"), strings.Join(a.OptDepends, "  "))
+	fmt.Println(boldWhiteFg("Conflicts With  :"), strings.Join(a.Conflicts, "  "))
+	fmt.Println(boldWhiteFg("Maintainer      :"), a.Maintainer)
+	fmt.Println(boldWhiteFg("Votes           :"), a.NumVotes)
+	fmt.Println(boldWhiteFg("Popularity      :"), a.Popularity)
 	if a.OutOfDate != 0 {
-		fmt.Println("\x1b[1;37mOut-of-date     :\x1b[0m", "Yes")
+		fmt.Println(boldWhiteFg("Out-of-date     :"), "Yes")
 	}
 
 	fmt.Println()
@@ -180,7 +150,7 @@ func biggestPackages() {
 	}
 
 	for i := 0; i < 10; i++ {
-		fmt.Printf("%s: \x1B[0;33m%s\x1B[0m\n", pkgS[i].Name(), human(pkgS[i].ISize()))
+		fmt.Println(pkgS[i].Name() + ": " + yellowFg(human(pkgS[i].ISize())))
 	}
 	// Could implement size here as well, but we just want the general idea
 }
@@ -198,15 +168,15 @@ func localStatistics() error {
 	}
 
 	fmt.Printf("\n Yay version r%s\n", version)
-	fmt.Println("\x1B[1;34m===========================================\x1B[0m")
-	fmt.Printf("\x1B[1;32mTotal installed packages: \x1B[0;33m%d\x1B[0m\n", info.Totaln)
-	fmt.Printf("\x1B[1;32mTotal foreign installed packages: \x1B[0;33m%d\x1B[0m\n", len(remoteNames))
-	fmt.Printf("\x1B[1;32mExplicitly installed packages: \x1B[0;33m%d\x1B[0m\n", info.Expln)
-	fmt.Printf("\x1B[1;32mTotal Size occupied by packages: \x1B[0;33m%s\x1B[0m\n", human(info.TotalSize))
-	fmt.Println("\x1B[1;34m===========================================\x1B[0m")
-	fmt.Println("\x1B[1;32mTen biggest packages\x1B[0m")
+	fmt.Println(boldCyanFg("==========================================="))
+	fmt.Println(boldGreenFg("Total installed packages: ") + yellowFg(strconv.Itoa(info.Totaln)))
+	fmt.Println(boldGreenFg("Total foreign installed packages: ") + yellowFg(strconv.Itoa(len(remoteNames))))
+	fmt.Println(boldGreenFg("Explicitly installed packages: ") + yellowFg(strconv.Itoa(info.Expln)))
+	fmt.Println(boldGreenFg("Total Size occupied by packages: ") + yellowFg(human(info.TotalSize)))
+	fmt.Println(boldCyanFg("==========================================="))
+	fmt.Println(boldGreenFg("Ten biggest packages"))
 	biggestPackages()
-	fmt.Println("\x1B[1;34m===========================================\x1B[0m")
+	fmt.Println(boldCyanFg("==========================================="))
 
 	var q aurQuery
 	var j int
@@ -242,15 +212,18 @@ func localStatistics() error {
 
 	for _, res := range q {
 		if res.Maintainer == "" {
-			fmt.Printf("\x1b[1;31;40mWarning: \x1B[1;33;40m%s\x1b[0;37;40m is orphaned.\x1b[0m\n", res.Name)
+			fmt.Println(boldRedFgBlackBg(arrow+"Warning:"),
+				boldYellowFgBlackBg(res.Name), whiteFgBlackBg("is orphaned"))
 		}
 		if res.OutOfDate != 0 {
-			fmt.Printf("\x1b[1;31;40mWarning: \x1B[1;33;40m%s\x1b[0;37;40m is out-of-date in AUR.\x1b[0m\n", res.Name)
+			fmt.Println(boldRedFgBlackBg(arrow+"Warning:"),
+				boldYellowFgBlackBg(res.Name), whiteFgBlackBg("is out-of-date in AUR"))
 		}
 	}
 
 	for _, res := range outcast {
-		fmt.Printf("\x1b[1;31;40mWarning: \x1B[1;33;40m%s\x1b[0;37;40m is not available in AUR.\x1b[0m\n", res)
+		fmt.Println(boldRedFgBlackBg(arrow+"Warning:"),
+			boldYellowFgBlackBg(res), whiteFgBlackBg("is not available in AUR"))
 	}
 
 	return nil
@@ -296,4 +269,100 @@ func printUpdateList() error {
 	}
 
 	return nil
+}
+
+func blackBg(in string) string {
+	if alpmConf.Options&alpm.ConfColor > 0 {
+		return "\x1b[0;;40m" + in + "\x1b[0m"
+	}
+
+	return in
+}
+
+func redFg(in string) string {
+	if alpmConf.Options&alpm.ConfColor > 0 {
+		return "\x1b[0;31m" + in + "\x1b[0m"
+	}
+
+	return in
+}
+
+func yellowFg(in string) string {
+	if alpmConf.Options&alpm.ConfColor > 0 {
+		return "\x1b[0;33m" + in + "\x1b[0m"
+	}
+
+	return in
+}
+
+func boldGreenFg(in string) string {
+	if alpmConf.Options&alpm.ConfColor > 0 {
+		return "\x1b[1;32m" + in + "\x1b[0m"
+	}
+
+	return in
+}
+
+func boldYellowFg(in string) string {
+	if alpmConf.Options&alpm.ConfColor > 0 {
+		return "\x1b[1;33m" + in + "\x1b[0m"
+	}
+
+	return in
+}
+
+func boldCyanFg(in string) string {
+	if alpmConf.Options&alpm.ConfColor > 0 {
+		return "\x1b[1;36m" + in + "\x1b[0m"
+	}
+
+	return in
+}
+
+func boldWhiteFg(in string) string {
+	if alpmConf.Options&alpm.ConfColor > 0 {
+		return "\x1b[1;37m" + in + "\x1b[0m"
+	}
+
+	return in
+}
+
+func redFgBlackBg(in string) string {
+	if alpmConf.Options&alpm.ConfColor > 0 {
+		return "\x1b[31;40m" + in + "\x1b[0m"
+	}
+
+	return in
+}
+
+func greenFgBlackBg(in string) string {
+	if alpmConf.Options&alpm.ConfColor > 0 {
+		return "\x1b[32;40m" + in + "\x1b[0m"
+	}
+
+	return in
+}
+
+func whiteFgBlackBg(in string) string {
+	if alpmConf.Options&alpm.ConfColor > 0 {
+		return "\x1b[0;37;40m" + in + "\x1b[0m"
+	}
+
+	return in
+}
+
+func boldRedFgBlackBg(in string) string {
+	if alpmConf.Options&alpm.ConfColor > 0 {
+		return "\x1b[1;31;40m" + in + "\x1b[0m"
+	}
+
+	return in
+}
+
+func boldYellowFgBlackBg(in string) string {
+	if alpmConf.Options&alpm.ConfColor > 0 {
+		return "\x1b[1;33;40m" + in + "\x1b[0m"
+	}
+
+	return in
 }

upgrade.go
@@ -64,28 +64,30 @@ func (u upSlice) Print(start int) {
 		new, errNew := pkgb.NewCompleteVersion(i.RemoteVersion)
 		var left, right string
 
-		f := func(name string) (color int) {
+		f := func(name string) (output string) {
 			var hash = 5381
 			for i := 0; i < len(name); i++ {
 				hash = int(name[i]) + ((hash << 5) + (hash))
 			}
-			return hash%6 + 31
+			return fmt.Sprintf("\x1b[1;%dm%s\x1b[0m", hash%6+31, name)
 		}
-		fmt.Printf("\x1b[33m%-2d\x1b[0m ", len(u)+start-k-1)
-		fmt.Printf("\x1b[1;%dm%s\x1b[0m/\x1b[1;39m%-25s\t\t\x1b[0m", f(i.Repository), i.Repository, i.Name)
+		fmt.Printf("%2s ", yellowFg(strconv.Itoa(len(u)+start-k-1)))
+		fmt.Print(f(i.Repository), "/", boldWhiteFg(i.Name))
 
 		if errOld != nil {
-			left = fmt.Sprintf("\x1b[31m%20s\x1b[0m", "Invalid Version")
+			left = redFg("Invalid Version")
 		} else {
-			left = fmt.Sprintf("\x1b[31m%18s\x1b[0m-%s", old.Version, old.Pkgrel)
+			left = redFg(string(old.Version)) + "-" + string(old.Pkgrel)
 		}
 
 		if errNew != nil {
-			right = fmt.Sprintf("\x1b[31m%s\x1b[0m", "Invalid Version")
+			right = redFg("Invalid Version")
 		} else {
-			right = fmt.Sprintf("\x1b[31m%s\x1b[0m-%s", new.Version, new.Pkgrel)
+			right = redFg(string(new.Version)) + "-" + string(new.Pkgrel)
 		}
-		fmt.Printf("%s -> %s\n", left, right)
+		w := 100 - len(i.Repository) - len(i.Name) - len(left)
+		fmt.Printf(fmt.Sprintf("%%%ds", w),
+			fmt.Sprintf("%s -> %s\n", left, right))
 	}
 }
 
@@ -151,7 +153,8 @@ func upDevel(remote []alpm.Package, packageC chan upgrade, done chan bool) {
 			}
 			if found {
 				if pkg.ShouldIgnore() {
-					fmt.Printf("\x1b[33mwarning:\x1b[0m %s ignoring package upgrade (%s => %s)\n", pkg.Name(), pkg.Version(), "git")
+					fmt.Print(warning)
+					fmt.Printf("%s ignoring package upgrade (%s => %s)\n", pkg.Name(), pkg.Version(), "git")
 				} else {
 					packageC <- upgrade{e.Package, "devel", e.SHA[0:6], "git"}
 				}
@@ -208,7 +211,8 @@ func upAUR(remote []alpm.Package, remoteNames []string) (toUpgrade upSlice, err 
 					if (config.TimeUpdate && (int64(qtemp[x].LastModified) > local[i].BuildDate().Unix())) ||
 						(alpm.VerCmp(local[i].Version(), qtemp[x].Version) < 0) {
 						if local[i].ShouldIgnore() {
-							fmt.Printf("\x1b[33mwarning:\x1b[0m %s ignoring package upgrade (%s => %s)\n", local[i].Name(), local[i].Version(), qtemp[x].Version)
+							fmt.Print(warning)
+							fmt.Printf("%s ignoring package upgrade (%s => %s)\n", local[i].Name(), local[i].Version(), qtemp[x].Version)
 						} else {
 							packageC <- upgrade{qtemp[x].Name, "aur", local[i].Version(), qtemp[x].Version}
 						}
@@ -255,7 +259,8 @@ func upRepo(local []alpm.Package) (upSlice, error) {
 		newPkg := pkg.NewVersion(dbList)
 		if newPkg != nil {
 			if pkg.ShouldIgnore() {
-				fmt.Printf("\x1b[33mwarning:\x1b[0m %s ignoring package upgrade (%s => %s)\n", pkg.Name(), pkg.Version(), newPkg.Version())
+				fmt.Print(warning)
+				fmt.Printf("%s ignoring package upgrade (%s => %s)\n", pkg.Name(), pkg.Version(), newPkg.Version())
 			} else {
 				slice = append(slice, upgrade{pkg.Name(), newPkg.DB().Name(), pkg.Version(), newPkg.Version()})
 			}

install.go
@@ -58,13 +58,15 @@ func install(parser *arguments) error {
 
 		for _, pkg := range dc.AurMake {
 			if pkg.Maintainer == "" {
-				fmt.Printf("\x1b[1;31;40m==> Warning:\x1b[0;;40m %s is orphaned.\x1b[0m\n", pkg.Name+"-"+pkg.Version)
+				fmt.Println(boldRedFgBlackBg(arrow+"Warning:"),
+					blackBg(pkg.Name+"-"+pkg.Version+"is orphaned"))
 			}
 		}
 
 		for _, pkg := range dc.Aur {
 			if pkg.Maintainer == "" {
-				fmt.Printf("\x1b[1;31;40m==> Warning:\x1b[0;;40m %s is orphaned.\x1b[0m\n", pkg.Name+"-"+pkg.Version)
+				fmt.Println(boldRedFgBlackBg(arrow+"Warning:"),
+					blackBg(pkg.Name+"-"+pkg.Version+"is orphaned"))
 			}
 		}
 

query.go
@@ -81,7 +81,7 @@ func (q aurQuery) missingPackage(pkgS []string) {
 		}
 
 		if !found {
-			fmt.Println("\x1b[31mUnable to find", depName, "in AUR\x1b[0m")
+			fmt.Println(redFg("Unable to find" + depName + "in AUR"))
 		}
 	}
 }
@@ -303,7 +303,7 @@ func hangingPackages() (hanging []string, err error) {
 		requiredby := pkg.ComputeRequiredBy()
 		if len(requiredby) == 0 {
 			hanging = append(hanging, pkg.Name())
-			fmt.Printf("%s: \x1B[0;33m%s\x1B[0m\n", pkg.Name(), human(pkg.ISize()))
+			fmt.Println(pkg.Name() + ": " + yellowFg(human(pkg.ISize())))
 
 		}
 		return nil
@@ -372,7 +372,7 @@ big:
 			requiredby := pkg.ComputeRequiredBy()
 			if len(requiredby) == 0 {
 				hanging = append(hanging, pkgName)
-				fmt.Printf("%s: \x1B[0;33m%s\x1B[0m\n", pkg.Name(), human(pkg.ISize()))
+				fmt.Println(pkg.Name() + ": " + yellowFg(human(pkg.ISize())))
 			}
 		}
 	}

commit 91757f51c7d54a44920c2e51152147b93fa7839a

Fix formatting on upgrade menu
commit 5286f385 broke the alignment on -> in the upgrade menu.

This commit moves the function responsable for printing the upgrade menu
from upgrade.go to print.go, fixes the formating to how it was before
and changes the padding on the number to three digits instead of two.
This allows proper alignment on numbers >100 which is not unheard of.

print.go
@@ -118,6 +118,21 @@ func formatPkgbase(pkg *rpc.Pkg, bases map[string][]*rpc.Pkg) string {
 	return str
 }
 
+// Print prints the details of the packages to upgrade.
+func (u upSlice) Print(start int) {
+	for k, i := range u {
+		left, right := getVersionDiff(i.LocalVersion, i.RemoteVersion)
+
+		fmt.Print(magenta(fmt.Sprintf("%3d ", len(u)+start-k-1)))
+		fmt.Print(bold(colourHash(i.Repository)), "/", cyan(i.Name))
+
+		w := 70 - len(i.Repository) - len(i.Name)
+		padding := fmt.Sprintf("%%%ds", w)
+		fmt.Printf(padding, left)
+		fmt.Printf(" -> %s\n", right)
+	}
+}
+
 // printDownloadsFromRepo prints repository packages to be downloaded
 func printDepCatagories(dc *depCatagories) {
 	repo := ""

upgrade.go
@@ -80,20 +80,6 @@ func getVersionDiff(oldVersion, newversion string) (left, right string) {
 	return
 }
 
-// Print prints the details of the packages to upgrade.
-func (u upSlice) Print(start int) {
-	for k, i := range u {
-		left, right := getVersionDiff(i.LocalVersion, i.RemoteVersion)
-
-		fmt.Print(magenta(fmt.Sprintf("%2d ", len(u)+start-k-1)))
-		fmt.Print(bold(colourHash(i.Repository)), "/", cyan(i.Name))
-
-		w := 70 - len(i.Repository) - len(i.Name) + len(left)
-		fmt.Printf(fmt.Sprintf("%%%ds", w),
-			fmt.Sprintf("%s -> %s\n", left, right))
-	}
-}
-
 // upList returns lists of packages to upgrade from each source.
 func upList(dt *depTree) (aurUp upSlice, repoUp upSlice, err error) {
 	local, remote, _, remoteNames, err := filterPackages()