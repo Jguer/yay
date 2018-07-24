b3a53d613d270e8f62db688cfc1503399570e4e8
morganamilo <morganamilo@gmail.com>
2018-07-24 13:04:07 +0100

    Update vendored dependencies

--- vendor/github.com/mikkeloscar/aur/aur.go
+++ vendor/github.com/mikkeloscar/aur/aur.go
@@ -41,6 +41,7 @@ type Pkg struct {
 	Provides       []string `json:"Provides"`
 	Replaces       []string `json:"Replaces"`
 	OptDepends     []string `json:"OptDepends"`
+	Groups         []string `json:"Groups"`
 	License        []string `json:"License"`
 	Keywords       []string `json:"Keywords"`
 }
