6e0797a8c7729ed4d028f3cb3f5ccca3b7944c89
Jguer <me@jguer.space>
2018-09-05 09:21:11 +0100

    Set version manually for gcc-go
    Signed-off-by: Jguer <me@jguer.space>

--- config.go
+++ config.go
@@ -77,7 +77,7 @@ type Configuration struct {
 	UseAsk             bool   `json:"useask"`
 }
 
-var version = "8.1110"
+var version = "8.1115"
 
 // configFileName holds the name of the config file.
 const configFileName string = "config.json"
