d3a9082f2ad8c9ff534cdbc81fc5a0ae59254889
Jguer <me@jguer.space>
2018-12-07 13:56:00 +0000

    Update patch version of yay to reflect makefile changes

--- config.go
+++ config.go
@@ -78,7 +78,7 @@ type Configuration struct {
 	UseAsk             bool   `json:"useask"`
 }
 
-var version = "9.0.0"
+var version = "9.0.1"
 
 // configFileName holds the name of the config file.
 const configFileName string = "config.json"
