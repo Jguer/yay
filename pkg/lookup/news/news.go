e9a4eaf62bbbce214557b044a5f8670a9e316565
Jguer <me@jguer.space>
2019-08-28 09:28:41 +0100

    Restructured code in news. Rename versiondiff file

--- pkg/lookup/news/news.go
+++ pkg/lookup/news/news.go
@@ -18,68 +18,26 @@ import (
 	"github.com/Jguer/yay/v10/pkg/types"
 )
 
-type item struct {
-	Title       string `xml:"title"`
-	Link        string `xml:"link"`
-	Description string `xml:"description"`
-	PubDate     string `xml:"pubDate"`
-	Creator     string `xml:"dc:creator"`
-}
-
-func (item item) print(cmdArgs *types.Arguments, buildTime time.Time) {
-	var fd string
-	date, err := time.Parse(time.RFC1123Z, item.PubDate)
-
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	} else {
-		fd = text.FormatTime(int(date.Unix()))
-		if _, double, _ := cmdArgs.GetArg("news", "w"); !double && !buildTime.IsZero() {
-			if buildTime.After(date) {
-				return
-			}
-		}
+type (
+	channel struct {
+		Title         string `xml:"title"`
+		Link          string `xml:"link"`
+		Description   string `xml:"description"`
+		Language      string `xml:"language"`
+		Lastbuilddate string `xml:"lastbuilddate"`
+		Items         []item `xml:"item"`
 	}
-
-	fmt.Println(text.Bold(text.Magenta(fd)), text.Bold(strings.TrimSpace(item.Title)))
-	//fmt.Println(strings.TrimSpace(item.Link))
-
-	if !cmdArgs.ExistsArg("q", "quiet") {
-		desc := strings.TrimSpace(parseNews(item.Description))
-		fmt.Println(desc)
+	item struct {
+		Title       string `xml:"title"`
+		Link        string `xml:"link"`
+		Description string `xml:"description"`
+		PubDate     string `xml:"pubDate"`
+		Creator     string `xml:"dc:creator"`
 	}
-}
-
-type channel struct {
-	Title         string `xml:"title"`
-	Link          string `xml:"link"`
-	Description   string `xml:"description"`
-	Language      string `xml:"language"`
-	Lastbuilddate string `xml:"lastbuilddate"`
-	Items         []item `xml:"item"`
-}
-
-type rss struct {
-	Channel channel `xml:"channel"`
-}
-
-func lastBuildTime(alpmHandle *alpm.Handle) (time.Time, error) {
-	var lastTime time.Time
-
-	pkgs, _, _, _, err := query.FilterPackages(alpmHandle)
-	if err != nil {
-		return lastTime, err
+	rss struct {
+		Channel channel `xml:"channel"`
 	}
-
-	for _, pkg := range pkgs {
-		thisTime := pkg.BuildDate()
-		if thisTime.After(lastTime) {
-			lastTime = thisTime
-		}
-	}
-
-	return lastTime, nil
-}
+)
 
 func PrintFeed(cmdArgs *types.Arguments, alpmHandle *alpm.Handle, sortMode int) error {
 	resp, err := http.Get("https://archlinux.org/feeds/news")
@@ -117,6 +75,24 @@ func PrintFeed(cmdArgs *types.Arguments, alpmHandle *alpm.Handle, sortMode int) 
 	}
 
 	return nil
+}
+
+func lastBuildTime(alpmHandle *alpm.Handle) (time.Time, error) {
+	var lastTime time.Time
+
+	pkgs, _, _, _, err := query.FilterPackages(alpmHandle)
+	if err != nil {
+		return lastTime, err
+	}
+
+	for _, pkg := range pkgs {
+		thisTime := pkg.BuildDate()
+		if thisTime.After(lastTime) {
+			lastTime = thisTime
+		}
+	}
+
+	return lastTime, nil
 }
 
 // Crude html parsing, good enough for the arch news
@@ -180,4 +156,28 @@ func parseNews(str string) string {
 
 	buffer.WriteString("\x1b[0m")
 	return buffer.String()
+}
+
+func (item item) print(cmdArgs *types.Arguments, buildTime time.Time) {
+	var fd string
+	date, err := time.Parse(time.RFC1123Z, item.PubDate)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	} else {
+		fd = text.FormatTime(int(date.Unix()))
+		if _, double, _ := cmdArgs.GetArg("news", "w"); !double && !buildTime.IsZero() {
+			if buildTime.After(date) {
+				return
+			}
+		}
+	}
+
+	fmt.Println(text.Bold(text.Magenta(fd)), text.Bold(strings.TrimSpace(item.Title)))
+	//fmt.Println(strings.TrimSpace(item.Link))
+
+	if !cmdArgs.ExistsArg("q", "quiet") {
+		desc := strings.TrimSpace(parseNews(item.Description))
+		fmt.Println(desc)
+	}
 }
