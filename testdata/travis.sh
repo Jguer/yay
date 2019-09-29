dfef396d3cdef11435d72026ea8ea6c83d2f1971
Jguer <me@jguer.space>
2019-09-29 12:01:17 +0100

    Upload yay release artifact

--- testdata/travis.sh
+++ testdata/travis.sh
@@ -6,6 +6,8 @@ set -evx
 
 export VERSION=$(git describe --long --tags | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g')
 export ARCH="x86_64"
+echo '::set-env name=VERSION::$VERSION'
+echo '::set-env name=ARCH::$ARCH'
 
 docker build --build-arg BUILD_ARCH=${ARCH} --target builder_env -t yay-builder_env .
 docker build --build-arg BUILD_ARCH=${ARCH} --target builder -t yay-builder .
@@ -25,9 +27,9 @@ docker build -t yay .
 # Create a release asset
 docker run --name artifact_factory yay-builder make release ARCH=${ARCH} VERSION=${VERSION}
 
-# Copy bin and release to workdir
-docker cp artifact_factory:/app/yay yay
-docker cp artifact_factory:/app/yay_${VERSION}_${ARCH}.tar.gz .
+# Copy bin and release to artifacts folder
+mkdir artifacts
+docker cp artifact_factory:/app/yay_${VERSION}_${ARCH}.tar.gz ./artifacts/
 
 # Cleanup docker
 docker rm artifact_factory
