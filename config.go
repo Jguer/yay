fc4b111be1f5e5f6d9af54e7fc4390e07987cf4d
Stefan Tatschner <stefan@rumpelsepp.org>
2019-10-21 12:47:26 +0200

    Remove delta support for pacman 5.2.0

--- config.go
+++ config.go
@@ -428,10 +428,6 @@ func configureAlpm(conf *pacmanconf.Config) error {
 		return err
 	}*/
 
-	if err := alpmHandle.SetDeltaRatio(pacmanConf.UseDelta); err != nil {
-		return err
-	}
-
 	if err := alpmHandle.SetUseSyslog(pacmanConf.UseSyslog); err != nil {
 		return err
 	}
