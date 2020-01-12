a0d831180d1f30b547ea1f7a24c0e58928e77ed6
Jguer <me@jguer.space>
2020-01-12 18:05:56 +0100

    fix(release): fix tag value sharing

--- testdata/ci/version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "::set-output name=CHANGELOG::$(git log --pretty=format:'%s%n%b==============================================' --abbrev-commit $1..$2)"
-
-if git describe --tags --exact-match; then
-    echo "::set-output name=VERSION::$(git describe --tags --exact-match | sed 's/^v//g')"
-else
-    echo "::set-output name=VERSION::$(git describe --long --tags | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g')"
-fi
