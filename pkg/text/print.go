5ae510ad6f763b0bca752ab1d824ce11176372ea
x-usr <70479203+x-usr@users.noreply.github.com>
2020-08-30 23:13:04 +0200

    fix(display): adapt padding to match pacman's.
    This change fixes part of #1332 related to splitting information into
    multiple lines.
    Change is implemented based on current pacman's code.

--- pkg/text/print.go
+++ pkg/text/print.go
@@ -3,8 +3,12 @@ package text
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
+	"syscall"
 
 	"github.com/leonelquinteros/gotext"
+	"golang.org/x/sys/unix"
 )
 
 const (
@@ -12,6 +16,8 @@ const (
 	smallArrow = " ->"
 	opSymbol   = "::"
 )
+
+var cachedColumnCount = -1
 
 func OperationInfoln(a ...interface{}) {
 	fmt.Fprint(os.Stdout, append([]interface{}{Bold(Cyan(opSymbol + " ")), boldCode}, a...)...)
@@ -59,10 +65,47 @@ func Errorln(a ...interface{}) {
 	fmt.Fprintln(os.Stderr, append([]interface{}{Bold(Red(smallArrow))}, a...)...)
 }
 
-func PrintInfoValue(str, value string) {
-	if value == "" {
-		value = gotext.Get("None")
+func getColumnCount() int {
+	if cachedColumnCount > 0 {
+		return cachedColumnCount
+	}
+	if count, err := strconv.Atoi(os.Getenv("COLUMNS")); err == nil {
+		cachedColumnCount = count
+		return cachedColumnCount
+	}
+	if ws, err := unix.IoctlGetWinsize(syscall.Stdout, unix.TIOCGWINSZ); err == nil {
+		cachedColumnCount = int(ws.Col)
+		return cachedColumnCount
+	}
+	return 80
+}
+
+func PrintInfoValue(key string, values ...string) {
+	// 16 (text) + 1 (:) + 1 ( )
+	const (
+		keyLength  = 18
+		delimCount = 2
+	)
+
+	str := fmt.Sprintf(Bold("%-16s: "), key)
+	if len(values) == 0 || (len(values) == 1 && values[0] == "") {
+		fmt.Fprintf(os.Stdout, "%s%s\n", str, gotext.Get("None"))
+		return
 	}
 
-	fmt.Fprintf(os.Stdout, Bold("%-16s%s")+" %s\n", str, ":", value)
+	maxCols := getColumnCount()
+	cols := keyLength + len(values[0])
+	str += values[0]
+	for _, value := range values[1:] {
+		if maxCols > keyLength && cols+len(value)+delimCount >= maxCols {
+			cols = keyLength
+			str += "\n" + strings.Repeat(" ", keyLength)
+		} else if cols != keyLength {
+			str += strings.Repeat(" ", delimCount)
+			cols += delimCount
+		}
+		str += value
+		cols += len(value)
+	}
+	fmt.Println(str)
 }
