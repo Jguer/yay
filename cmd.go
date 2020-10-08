179520438f0f51930771458a0c51373ff607e9ae
FuRuYa7 <goo.sirocat47@gmail.com>
2020-10-09 01:11:56 +0900

    <Fixed 5> FuRuYa7-develop/cmd.go -- Fixed '<package (s)>'

--- cmd.go
+++ cmd.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	alpm "github.com/Jguer/go-alpm/v2"
 	"github.com/leonelquinteros/gotext"
@@ -19,125 +20,160 @@ import (
 	"github.com/Jguer/yay/v10/pkg/vcs"
 )
 
+func opHelp(operation string, args ...string) string {
+	newArgs := make([]string, 0, len(args))
+	for _, arg := range args {
+		newArgs = append(newArgs, fmt.Sprintf("[%s]", arg))
+	}
+	return fmt.Sprintf("    yay %-18s %s", operation, strings.Join(newArgs, " "))
+}
+
+func opHlp2(operation string, args ...string) string {
+	newArgs := make([]string, 0, len(args))
+	for _, arg := range args {
+		newArgs = append(newArgs, fmt.Sprintf("<%s>", arg))
+	}
+	return fmt.Sprintf("    yay %-18s %s", operation, strings.Join(newArgs, " "))
+}
+
+func optionHelp(option, description string, args ...string) string {
+	newArgs := make([]string, 0, len(args))
+	for _, arg := range args {
+		newArgs = append(newArgs, fmt.Sprintf("<%s>", arg))
+	}
+	return fmt.Sprintf("    %-18s %-2s %s", option, strings.Join(newArgs, " "), description)
+}
+
+func optionHlp2(option, description string, args ...string) string {
+	newArgs := make([]string, 0, len(args))
+	for _, arg := range args {
+		newArgs = append(newArgs, fmt.Sprintf("<%s>", arg))
+	}
+	return fmt.Sprintf("    %-13s %-7s %s", option, strings.Join(newArgs, " "), description)
+}
+
+func optionHlp3(option, description string, args ...string) string {
+	newArgs := make([]string, 0, len(args))
+	for _, arg := range args {
+		newArgs = append(newArgs, fmt.Sprintf("<%s>", arg))
+	}
+	return fmt.Sprintf("    %-15s %-5s %s", option, strings.Join(newArgs, " "), description)
+}
+
 func usage() {
-	fmt.Println(`Usage:
-    yay
-    yay <operation> [...]
-    yay <package(s)>
+	fmt.Print(gotext.Get("Usage"), ":\n    yay\n")
+	fmt.Printf("    yay <%s> [...]\n", gotext.Get("operation"))
+	fmt.Printf("    yay <%s>\n", gotext.Get("package(s)"))
 
-operations:
-    yay {-h --help}
-    yay {-V --version}
-    yay {-D --database}    <options> <package(s)>
-    yay {-F --files}       [options] [package(s)]
-    yay {-Q --query}       [options] [package(s)]
-    yay {-R --remove}      [options] <package(s)>
-    yay {-S --sync}        [options] [package(s)]
-    yay {-T --deptest}     [options] [package(s)]
-    yay {-U --upgrade}     [options] <file(s)>
+	fmt.Print("\n", gotext.Get("operations"), ":\n")
+	fmt.Println(opHelp("{-h --help}"))
+	fmt.Println(opHelp("{-V --version}"))
+	fmt.Println(opHlp2("{-D --database}", gotext.Get("options"), gotext.Get("package(s)")))
+	fmt.Println(opHelp("{-F --files}", gotext.Get("options"), gotext.Get("package(s)")))
+	fmt.Println(opHelp("{-Q --query}", gotext.Get("options"), gotext.Get("package(s)")))
+	fmt.Println(opHelp("{-R --remove}", gotext.Get("options")), "<"+gotext.Get("package(s)")+">")
+	fmt.Println(opHelp("{-S --sync}", gotext.Get("options"), gotext.Get("package(s)")))
+	fmt.Println(opHelp("{-T --deptest}", gotext.Get("options"), gotext.Get("package(s)")))
+	fmt.Println(opHelp("{-U --upgrade}", gotext.Get("options")), "<"+gotext.Get("file(s)")+">")
 
-New operations:
-    yay {-Y --yay}         [options] [package(s)]
-    yay {-P --show}        [options]
-    yay {-G --getpkgbuild} [package(s)]
+	fmt.Print("\n", gotext.Get("New operations"), ":\n")
+	fmt.Println(opHelp("{-G --getpkgbuild}", gotext.Get("package(s)")))
+	fmt.Println(opHelp("{-P --show}", gotext.Get("options")))
+	fmt.Println(opHelp("{-Y --yay}", gotext.Get("options"), gotext.Get("package(s)")))
 
-If no arguments are provided 'yay -Syu' will be performed
-If no operation is provided -Y will be assumed
+	fmt.Println()
+	fmt.Println(gotext.Get("If no arguments are provided 'yay -Syu' will be performed"))
+	fmt.Println(gotext.Get("If no operation is provided -Y will be assumed"))
 
-New options:
-       --repo             Assume targets are from the repositories
-    -a --aur              Assume targets are from the AUR
+	fmt.Print("\n", gotext.Get("New options"), ":\n")
+	fmt.Println(optionHelp("--repo", gotext.Get("Assume targets are from the repositories")))
+	fmt.Println(optionHelp("-a --aur", gotext.Get("Assume targets are from the AUR")))
 
-Permanent configuration options:
-    --save                Causes the following options to be saved back to the
-                          config file when used
+	fmt.Print("\n", gotext.Get("Permanent configuration options"), ":\n")
+	fmt.Println(optionHelp("--save", gotext.Get("Causes the following options to be saved back to the")))
+	fmt.Println(optionHelp("      ", gotext.Get("config file when used")))
 
-    --aururl      <url>   Set an alternative AUR URL
-    --builddir    <dir>   Directory used to download and run PKGBUILDS
-    --absdir      <dir>   Directory used to store downloads from the ABS
-    --editor      <file>  Editor to use when editing PKGBUILDs
-    --editorflags <flags> Pass arguments to editor
-    --makepkg     <file>  makepkg command to use
-    --mflags      <flags> Pass arguments to makepkg
-    --pacman      <file>  pacman command to use
-    --git         <file>  git command to use
-    --gitflags    <flags> Pass arguments to git
-    --gpg         <file>  gpg command to use
-    --gpgflags    <flags> Pass arguments to gpg
-    --config      <file>  pacman.conf file to use
-    --makepkgconf <file>  makepkg.conf file to use
-    --nomakepkgconf       Use the default makepkg.conf
+	fmt.Println()
+	fmt.Println(optionHlp2("--aururl", gotext.Get("Set an alternative AUR URL"), gotext.Get("url")))
+	fmt.Println(optionHlp2("--builddir", gotext.Get("Directory used to download and run PKGBUILDS"), gotext.Get("dir")))
+	fmt.Println(optionHlp2("--absdir", gotext.Get("Directory used to store downloads from the ABS"), gotext.Get("dir")))
+	fmt.Println(optionHlp2("--editor", gotext.Get("Editor to use when editing PKGBUILDs"), gotext.Get("file")))
+	fmt.Println(optionHlp2("--editorflags", gotext.Get("Pass arguments to editor"), gotext.Get("flags")))
+	fmt.Println(optionHlp2("--makepkg", gotext.Get("makepkg command to use"), gotext.Get("file")))
+	fmt.Println(optionHlp2("--mflags", gotext.Get("Pass arguments to makepkg"), gotext.Get("flags")))
+	fmt.Println(optionHlp2("--pacman", gotext.Get("pacman command to use"), gotext.Get("file")))
+	fmt.Println(optionHlp2("--git", gotext.Get("git command to use"), gotext.Get("file")))
+	fmt.Println(optionHlp2("--gitflags", gotext.Get("Pass arguments to git"), gotext.Get("flags")))
+	fmt.Println(optionHlp2("--gpg", gotext.Get("gpg command to use"), gotext.Get("file")))
+	fmt.Println(optionHlp2("--gpgflags", gotext.Get("Pass arguments to gpg"), gotext.Get("flags")))
+	fmt.Println(optionHlp2("--config", gotext.Get("pacman.conf file to use"), gotext.Get("file")))
+	fmt.Println(optionHlp2("--makepkgconf", gotext.Get("makepkg.conf file to use"), gotext.Get("file")))
+	fmt.Println(optionHelp("--nomakepkgconf", gotext.Get("Use the default makepkg.conf")))
+	fmt.Println(optionHlp3("--requestsplitn", gotext.Get("Max amount of packages to query per AUR request"), "n"))
+	fmt.Println(optionHelp("--completioninterval ", gotext.Get("Time in days to refresh completion cache"), "n"))
+	fmt.Println(optionHlp2("--sortby", gotext.Get("Sort AUR results by a specific field during search"), gotext.Get("field")))
+	fmt.Println(optionHlp2("--searchby", gotext.Get("Search for packages using a specified field"), gotext.Get("field")))
+	fmt.Println(optionHlp3("--answerclean", gotext.Get("Set a predetermined answer for the clean build menu"), "a"))
+	fmt.Println(optionHlp3("--answerdiff", gotext.Get("Set a predetermined answer for the diff menu"), "a"))
+	fmt.Println(optionHlp3("--answeredit", gotext.Get("Set a predetermined answer for the edit pkgbuild menu"), "a"))
+	fmt.Println(optionHlp3("--answerupgrade", gotext.Get("Set a predetermined answer for the upgrade menu"), "a"))
+	fmt.Println(optionHelp("--noanswerclean", gotext.Get("Unset the answer for the clean build menu")))
+	fmt.Println(optionHelp("--noanswerdiff", gotext.Get("Unset the answer for the edit diff menu")))
+	fmt.Println(optionHelp("--noansweredit", gotext.Get("Unset the answer for the edit pkgbuild menu")))
+	fmt.Println(optionHelp("--noanswerupgrade", gotext.Get("Unset the answer for the upgrade menu")))
+	fmt.Println(optionHelp("--cleanmenu", gotext.Get("Give the option to clean build PKGBUILDS")))
+	fmt.Println(optionHelp("--diffmenu", gotext.Get("Give the option to show diffs for build files")))
+	fmt.Println(optionHelp("--editmenu", gotext.Get("Give the option to edit/view PKGBUILDS")))
+	fmt.Println(optionHelp("--upgrademenu", gotext.Get("Show a detailed list of updates with the option to skip any")))
+	fmt.Println(optionHelp("--nocleanmenu", gotext.Get("Don't clean build PKGBUILDS")))
+	fmt.Println(optionHelp("--nodiffmenu", gotext.Get("Don't show diffs for build files")))
+	fmt.Println(optionHelp("--noeditmenu", gotext.Get("Don't edit/view PKGBUILDS")))
+	fmt.Println(optionHelp("--noupgrademenu", gotext.Get("Don't show the upgrade menu")))
+	fmt.Println(optionHelp("--askremovemake", gotext.Get("Ask to remove makedepends after install")))
+	fmt.Println(optionHelp("--removemake", gotext.Get("Remove makedepends after install")))
+	fmt.Println(optionHelp("--noremovemake", gotext.Get("Don't remove makedepends after install")))
+	fmt.Println(optionHelp("--cleanafter", gotext.Get("Remove package sources after successful install")))
+	fmt.Println(optionHelp("--nocleanafter", gotext.Get("Do not remove package sources after successful build")))
+	fmt.Println(optionHelp("--bottomup", gotext.Get("Shows AUR's packages first and then repository's")))
+	fmt.Println(optionHelp("--topdown", gotext.Get("Shows repository's packages first and then AUR's")))
+	fmt.Println(optionHelp("--devel", gotext.Get("Check development packages during sysupgrade")))
+	fmt.Println(optionHelp("--nodevel", gotext.Get("Do not check development packages")))
+	fmt.Println(optionHelp("--rebuild", gotext.Get("Always build target packages")))
+	fmt.Println(optionHelp("--rebuildall", gotext.Get("Always build all AUR packages")))
+	fmt.Println(optionHelp("--norebuild", gotext.Get("Skip package build if in cache and up to date")))
+	fmt.Println(optionHelp("--rebuildtree", gotext.Get("Always build all AUR packages even if installed")))
+	fmt.Println(optionHelp("--redownload", gotext.Get("Always download pkgbuilds of targets")))
+	fmt.Println(optionHelp("--noredownload", gotext.Get("Skip pkgbuild download if in cache and up to date")))
+	fmt.Println(optionHelp("--redownloadall", gotext.Get("Always download pkgbuilds of all AUR packages")))
+	fmt.Println(optionHelp("--provides", gotext.Get("Look for matching providers when searching for packages")))
+	fmt.Println(optionHelp("--noprovides", gotext.Get("Just look for packages by pkgname")))
+	fmt.Println(optionHelp("--pgpfetch", gotext.Get("Prompt to import PGP keys from PKGBUILDs")))
+	fmt.Println(optionHelp("--nopgpfetch", gotext.Get("Don't prompt to import PGP keys")))
+	fmt.Println(optionHelp("--useask", gotext.Get("Automatically resolve conflicts using pacman's ask flag")))
+	fmt.Println(optionHelp("--nouseask", gotext.Get("Confirm conflicts manually during the install")))
+	fmt.Println(optionHelp("--combinedupgrade", gotext.Get("Refresh then perform the repo and AUR upgrade together")))
+	fmt.Println(optionHelp("--batchinstall", gotext.Get("Build multiple AUR packages then install them together")))
+	fmt.Println(optionHelp("--nobatchinstall", gotext.Get("Build and install each AUR package one by one")))
+	fmt.Println(optionHlp2("--sudo", gotext.Get("sudo command to use"), gotext.Get("file")))
+	fmt.Println(optionHlp2("--sudoflags", gotext.Get("Pass arguments to sudo"), gotext.Get("flags")))
+	fmt.Println(optionHelp("--sudoloop", gotext.Get("Loop sudo calls in the background to avoid timeout")))
+	fmt.Println(optionHelp("--nosudoloop", gotext.Get("Do not loop sudo calls in the background")))
+	fmt.Println(optionHelp("--timeupdate", gotext.Get("Check packages' AUR page for changes during sysupgrade")))
+	fmt.Println(optionHelp("--notimeupdate", gotext.Get("Do not check packages' AUR page for changes")))
 
-    --requestsplitn <n>   Max amount of packages to query per AUR request
-    --completioninterval  <n> Time in days to refresh completion cache
-    --sortby    <field>   Sort AUR results by a specific field during search
-    --searchby  <field>   Search for packages using a specified field
-    --answerclean   <a>   Set a predetermined answer for the clean build menu
-    --answerdiff    <a>   Set a predetermined answer for the diff menu
-    --answeredit    <a>   Set a predetermined answer for the edit pkgbuild menu
-    --answerupgrade <a>   Set a predetermined answer for the upgrade menu
-    --noanswerclean       Unset the answer for the clean build menu
-    --noanswerdiff        Unset the answer for the edit diff menu
-    --noansweredit        Unset the answer for the edit pkgbuild menu
-    --noanswerupgrade     Unset the answer for the upgrade menu
-    --cleanmenu           Give the option to clean build PKGBUILDS
-    --diffmenu            Give the option to show diffs for build files
-    --editmenu            Give the option to edit/view PKGBUILDS
-    --upgrademenu         Show a detailed list of updates with the option to skip any
-    --nocleanmenu         Don't clean build PKGBUILDS
-    --nodiffmenu          Don't show diffs for build files
-    --noeditmenu          Don't edit/view PKGBUILDS
-    --noupgrademenu       Don't show the upgrade menu
-    --askremovemake       Ask to remove makedepends after install
-    --removemake          Remove makedepends after install
-    --noremovemake        Don't remove makedepends after install
+	fmt.Print("\n", gotext.Get("show specific options"), ":\n")
+	fmt.Println(optionHelp("-c --complete", gotext.Get("Used for completions")))
+	fmt.Println(optionHelp("-d --defaultconfig", gotext.Get("Print default yay configuration")))
+	fmt.Println(optionHelp("-g --currentconfig", gotext.Get("Print current yay configuration")))
+	fmt.Println(optionHelp("-s --stats", gotext.Get("Display system package statistics")))
+	fmt.Println(optionHelp("-w --news", gotext.Get("Print arch news")))
 
-    --cleanafter          Remove package sources after successful install
-    --nocleanafter        Do not remove package sources after successful build
-    --bottomup            Shows AUR's packages first and then repository's
-    --topdown             Shows repository's packages first and then AUR's
+	fmt.Print("\n", gotext.Get("yay specific options"), ":\n")
+	fmt.Println(optionHelp("-c --clean", gotext.Get("Remove unneeded dependencies")))
+	fmt.Println(optionHelp("--gendb", gotext.Get("Generates development package DB used for updating")))
 
-    --devel               Check development packages during sysupgrade
-    --nodevel             Do not check development packages
-    --rebuild             Always build target packages
-    --rebuildall          Always build all AUR packages
-    --norebuild           Skip package build if in cache and up to date
-    --rebuildtree         Always build all AUR packages even if installed
-    --redownload          Always download pkgbuilds of targets
-    --noredownload        Skip pkgbuild download if in cache and up to date
-    --redownloadall       Always download pkgbuilds of all AUR packages
-    --provides            Look for matching providers when searching for packages
-    --noprovides          Just look for packages by pkgname
-    --pgpfetch            Prompt to import PGP keys from PKGBUILDs
-    --nopgpfetch          Don't prompt to import PGP keys
-    --useask              Automatically resolve conflicts using pacman's ask flag
-    --nouseask            Confirm conflicts manually during the install
-    --combinedupgrade     Refresh then perform the repo and AUR upgrade together
-    --nocombinedupgrade   Perform the repo upgrade and AUR upgrade separately
-    --batchinstall        Build multiple AUR packages then install them together
-    --nobatchinstall      Build and install each AUR package one by one
-
-    --sudo                <file>  sudo command to use
-    --sudoflags           <flags> Pass arguments to sudo
-    --sudoloop            Loop sudo calls in the background to avoid timeout
-    --nosudoloop          Do not loop sudo calls in the background
-
-    --timeupdate          Check packages' AUR page for changes during sysupgrade
-    --notimeupdate        Do not check packages' AUR page for changes
-
-show specific options:
-    -c --complete         Used for completions
-    -d --defaultconfig    Print default yay configuration
-    -g --currentconfig    Print current yay configuration
-    -s --stats            Display system package statistics
-    -w --news             Print arch news
-
-yay specific options:
-    -c --clean            Remove unneeded dependencies
-       --gendb            Generates development package DB used for updating
-
-getpkgbuild specific options:
-    -f --force            Force download for existing ABS packages`)
+	fmt.Print("\n", gotext.Get("getpkgbuild specific options"), ":\n")
+	fmt.Println(optionHelp("-f --force", gotext.Get("Force download for existing ABS packages")))
 }
 
 func handleCmd(cmdArgs *settings.Arguments, dbExecutor db.Executor) error {
