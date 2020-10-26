632fc6ea7f75285197dbe1915b3a7a21b201588c
lwlf <wuhaojie_2011@sina.com>
2020-10-26 18:20:34 +0800

    unsaved config when yay's config path does not exist

--- pkg/settings/config.go
+++ pkg/settings/config.go
@@ -85,9 +85,12 @@ func (c *Configuration) Save(configPath string) error {
 	marshalledinfo = append(marshalledinfo, '\n')
 	// https://github.com/Jguer/yay/issues/1399
 	// fix: unsaved config when yay's config path does not exist
-	_, err = os.Stat(path.Dir(configPath))
+	_, err = os.Stat(filepath.Dir(configPath))
 	if os.IsNotExist(err) {
-		os.MkdirAll(path.Dir(configPath), 0761)
+		err := os.MkdirAll(path.Dir(configPath), 0761)
+		if err != nil {
+			return err
+		}
 	}
 	in, err := os.OpenFile(configPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0o644)
 	if err != nil {
