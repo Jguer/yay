7d080df6acc97174d6ab1d674106411cddf833f4
jguer <me@jguer.space>
2020-10-26 09:22:00 +0100

    fix(config): fix mflags and gitflags

--- pkg/settings/parser.go
+++ pkg/settings/parser.go
@@ -817,6 +817,8 @@ func (a *Arguments) ParseCommandLine(config *Configuration) error {
 	}
 
 	a.extractYayOptions(config)
+	config.Runtime.CmdBuilder.MakepkgFlags = strings.Split(config.MFlags, " ")
+	config.Runtime.CmdBuilder.GitFlags = strings.Split(config.GitFlags, " ")
 	return nil
 }
 
