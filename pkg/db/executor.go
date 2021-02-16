5993613c539e12ec81384b0e417912aa3958e004
Jannis M. Hoffmann <jannis.hoffmann@rwth-aachen.de>
2021-02-16 15:27:08 +0100

    Added type alias for alpm types in db

--- pkg/db/executor.go
+++ pkg/db/executor.go
@@ -8,26 +8,29 @@ import (
 	"github.com/Jguer/yay/v10/pkg/upgrade"
 )
 
+type IPackage = alpm.IPackage
+type Depend = alpm.Depend
+
 type Executor interface {
 	AlpmArch() (string, error)
-	BiggestPackages() []alpm.IPackage
+	BiggestPackages() []IPackage
 	Cleanup()
 	IsCorrectVersionInstalled(string, string) bool
 	LastBuildTime() time.Time
-	LocalPackage(string) alpm.IPackage
-	LocalPackages() []alpm.IPackage
+	LocalPackage(string) IPackage
+	LocalPackages() []IPackage
 	LocalSatisfierExists(string) bool
-	PackageConflicts(alpm.IPackage) []alpm.Depend
-	PackageDepends(alpm.IPackage) []alpm.Depend
-	SatisfierFromDB(string, string) alpm.IPackage
-	PackageGroups(alpm.IPackage) []string
-	PackageOptionalDepends(alpm.IPackage) []alpm.Depend
-	PackageProvides(alpm.IPackage) []alpm.Depend
-	PackagesFromGroup(string) []alpm.IPackage
+	PackageConflicts(IPackage) []Depend
+	PackageDepends(IPackage) []Depend
+	SatisfierFromDB(string, string) IPackage
+	PackageGroups(IPackage) []string
+	PackageOptionalDepends(IPackage) []Depend
+	PackageProvides(IPackage) []Depend
+	PackagesFromGroup(string) []IPackage
 	RefreshHandle() error
 	RepoUpgrades(bool) (upgrade.UpSlice, error)
-	SyncPackage(string) alpm.IPackage
-	SyncPackages(...string) []alpm.IPackage
-	SyncSatisfier(string) alpm.IPackage
+	SyncPackage(string) IPackage
+	SyncPackages(...string) []IPackage
+	SyncSatisfier(string) IPackage
 	SyncSatisfierExists(string) bool
 }
