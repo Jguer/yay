94cff87927f7f1da8da44554632c1cace63f6a57
Drumstickx <9080610+Drumstickx@users.noreply.github.com>
2022-03-31 14:19:43 +0200

    Fix ContinueTask user prompt for locales with uppercase yes (#1735)
    * Fix ContinueTask user prompt for locales with uppercase yes
    
    The yes/y strings have to be converted to lowercase to enable matching of
    the user input converted to lowercase. Otherwise, locales with uppercase
    yes/y strings (e.g., German: `Ja/J`) will never match.
    
    * Addess linter issue
    
    * Remove obsolete lowercase conversion

--- pkg/text/text.go
+++ pkg/text/text.go
@@ -74,7 +74,5 @@ func ContinueTask(s string, cont, noConfirm bool) bool {
 		return cont
 	}
 
-	response = strings.ToLower(response)
-
-	return response == yes || response == y
+	return strings.EqualFold(response, yes) || strings.EqualFold(response, y)
 }
