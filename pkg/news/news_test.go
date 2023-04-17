a3c3dd4b308f58829c3ae08745d366cfff8e4720
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2023-04-17 15:15:30 +0530

    Remove unnecessary environment reset

--- pkg/news/news_test.go
+++ pkg/news/news_test.go
@@ -121,8 +121,6 @@ func TestPrintNewsFeed(t *testing.T) {
 		{name: "latest-quiet", args: args{bottomUp: true, cutOffDate: lastNewsTime, all: false, quiet: true}, wantErr: false},
 		{name: "latest-quiet-topdown", args: args{bottomUp: false, cutOffDate: lastNewsTime, all: false, quiet: true}, wantErr: false},
 	}
-	currentTZ := os.Getenv("TZ")
-	defer os.Setenv("TZ", currentTZ)
 	os.Setenv("TZ", "UTC")
 	for _, tt := range tests {
 		tt := tt
