108430e24c35228bedaf2faf63a966954b8d9bb7
jguer <me@jguer.space>
2023-05-24 00:44:36 +0200

    remove unused menu fns

--- pkg/menus/clean_menu.go
+++ pkg/menus/clean_menu.go
@@ -3,7 +3,6 @@ package menus
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"os"
 
@@ -70,45 +69,6 @@ func CleanFn(ctx context.Context, config *settings.Configuration, w io.Writer, p
 			text.Warnln(gotext.Get("Unable to clean:"), dir)
 
 			return err
-		}
-	}
-
-	return nil
-}
-
-func Clean(w io.Writer, cleanMenuOption bool, pkgbuildDirs map[string]string,
-	installed mapset.Set[string], noConfirm bool, answerClean string,
-) error {
-	if !(cleanMenuOption && anyExistInCache(pkgbuildDirs)) {
-		return nil
-	}
-
-	skipFunc := func(pkg string) bool {
-		dir := pkgbuildDirs[pkg]
-		if _, err := os.Stat(dir); os.IsNotExist(err) {
-			return true
-		}
-
-		return false
-	}
-
-	bases := make([]string, 0, len(pkgbuildDirs))
-	for pkg := range pkgbuildDirs {
-		bases = append(bases, pkg)
-	}
-
-	toClean, errClean := selectionMenu(w, pkgbuildDirs, bases, installed, gotext.Get("Packages to cleanBuild?"),
-		noConfirm, answerClean, skipFunc)
-	if errClean != nil {
-		return errClean
-	}
-
-	for i, base := range toClean {
-		dir := pkgbuildDirs[base]
-		text.OperationInfoln(gotext.Get("Deleting (%d/%d): %s", i+1, len(toClean), text.Cyan(dir)))
-
-		if err := os.RemoveAll(dir); err != nil {
-			fmt.Fprintln(os.Stderr, err)
 		}
 	}
 
