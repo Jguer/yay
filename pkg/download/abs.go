3ba55f85db322c54e5bff5b4ce0880f43282fce3
moson-mo <mo-son@mailbox.org>
2023-05-24 22:47:22 +0200

    fix(pkgbuild): convert package name for gitlab URLs
    Convert package names to gitlab repo names
    
    Signed-off-by: moson-mo <mo-son@mailbox.org>

--- pkg/download/abs.go
+++ pkg/download/abs.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"regexp"
 
 	"github.com/leonelquinteros/gotext"
 
@@ -22,16 +23,39 @@ var (
 	ErrABSPackageNotFound = errors.New(gotext.Get("package not found in repos"))
 )
 
+type regexReplace struct {
+	repl  string
+	match *regexp.Regexp
+}
+
+// regex replacements for Gitlab URLs
+// info: https://gitlab.archlinux.org/archlinux/devtools/-/blob/6ce666a1669235749c17d5c44d8a24dea4a135da/src/lib/api/gitlab.sh#L84
+var gitlabRepl = []regexReplace{
+	{repl: `$1-$2`, match: regexp.MustCompile(`([a-zA-Z0-9]+)\+([a-zA-Z]+)`)},
+	{repl: `plus`, match: regexp.MustCompile(`\+`)},
+	{repl: `-`, match: regexp.MustCompile(`[^a-zA-Z0-9_\-.]`)},
+	{repl: `-`, match: regexp.MustCompile(`[_\-]{2,}`)},
+	{repl: `unix-tree`, match: regexp.MustCompile(`^tree$`)},
+}
+
 // Return format for pkgbuild
 // https://gitlab.archlinux.org/archlinux/packaging/packages/0ad/-/raw/main/PKGBUILD
 func getPackagePKGBUILDURL(pkgName string) string {
-	return fmt.Sprintf("%s/%s/-/raw/main/PKGBUILD", absPackageURL, pkgName)
+	return fmt.Sprintf("%s/%s/-/raw/main/PKGBUILD", absPackageURL, convertPkgNameForURL(pkgName))
 }
 
 // Return format for pkgbuild repo
 // https://gitlab.archlinux.org/archlinux/packaging/packages/0ad.git
 func getPackageRepoURL(pkgName string) string {
-	return fmt.Sprintf("%s/%s.git", absPackageURL, pkgName)
+	return fmt.Sprintf("%s/%s.git", absPackageURL, convertPkgNameForURL(pkgName))
+}
+
+// convert pkgName for Gitlab URL path (repo name)
+func convertPkgNameForURL(pkgName string) string {
+	for _, regex := range gitlabRepl {
+		pkgName = regex.match.ReplaceAllString(pkgName, regex.repl)
+	}
+	return pkgName
 }
 
 // ABSPKGBUILD retrieves the PKGBUILD file to a dest directory.
