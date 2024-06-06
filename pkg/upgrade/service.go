88f1780f2b60e3f879377d795718ec9037662b7e
iTrooz <hey@itrooz.fr>
2024-06-06 16:20:25 +0200

    add comments

--- pkg/upgrade/service.go
+++ pkg/upgrade/service.go
@@ -302,24 +302,29 @@ func (u *UpgradeService) UserExcludeUpgrades(graph *topo.Graph[string, *dep.Inst
 	// upgrade menu asks you which packages to NOT upgrade so in this case
 	// exclude and include are kind of swapped
 	exclude, include, otherExclude, otherInclude := intrange.ParseNumberMenu(numbers)
+
+	// true if user doesn't want to include specific packages
 	isInclude := len(include) == 0 && otherInclude.Cardinality() == 0
 
 	excluded := make([]string, 0)
 	for i := range allUp.Up {
 		up := &allUp.Up[i]
 
+		// exclude repositories mentionned by the user
 		if isInclude && otherExclude.Contains(up.Repository) {
 			u.log.Debugln("pruning", up.Name)
 			excluded = append(excluded, graph.Prune(up.Name)...)
 			continue
 		}
 
+		// exclude packages mentionned by the user
 		if isInclude && exclude.Get(len(allUp.Up)-i) {
 			u.log.Debugln("pruning", up.Name)
 			excluded = append(excluded, graph.Prune(up.Name)...)
 			continue
 		}
 
+		// If the user explicitely wants to include a package/repository, exclude everything else
 		if !isInclude && !(include.Get(len(allUp.Up)-i) || otherInclude.Contains(up.Repository)) {
 			u.log.Debugln("pruning", up.Name)
 			excluded = append(excluded, graph.Prune(up.Name)...)
