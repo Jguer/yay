bea53a4a09526ace9a03f33cd5cf8eb3f48fa188
Absobel <107758617+Absobel@users.noreply.github.com>
2024-09-19 14:15:44 +0200

    fix: added missing spaces (#2386)

--- pkg/dep/topo/errors.go
+++ pkg/dep/topo/errors.go
@@ -3,7 +3,7 @@ package topo
 import "errors"
 
 var (
-	ErrSelfReferential  = errors.New("self-referential dependencies not allowed")
-	ErrConflictingAlias = errors.New("alias already defined")
-	ErrCircular         = errors.New("circular dependencies not allowed")
+	ErrSelfReferential  = errors.New(" self-referential dependencies not allowed")
+	ErrConflictingAlias = errors.New(" alias already defined")
+	ErrCircular         = errors.New(" circular dependencies not allowed")
 )
