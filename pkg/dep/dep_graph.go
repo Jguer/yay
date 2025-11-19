2e7b26aae2871f40900bd85c622c616635d87efd
sarbojitrana <sarbojitrana47c@gmail.com>
2025-11-19 21:33:00 +0530

    fix: Correctly return exit status 1 when no packages are found

--- pkg/dep/dep_graph.go
+++ pkg/dep/dep_graph.go
@@ -15,6 +15,7 @@ import (
 	"github.com/Jguer/yay/v12/pkg/dep/topo"
 	"github.com/Jguer/yay/v12/pkg/intrange"
 	aur "github.com/Jguer/yay/v12/pkg/query"
+	query "github.com/Jguer/yay/v12/pkg/query"
 	"github.com/Jguer/yay/v12/pkg/text"
 )
 
@@ -422,6 +423,8 @@ func (g *Grapher) GraphFromAUR(ctx context.Context,
 
 	aurPkgsAdded := []*aurc.Pkg{}
 
+	var packagesNotFound int
+
 	for _, target := range targets {
 		if cachedProvidePkg, ok := g.providerCache[target]; ok {
 			aurPkgs = cachedProvidePkg
@@ -435,6 +438,7 @@ func (g *Grapher) GraphFromAUR(ctx context.Context,
 
 		if len(aurPkgs) == 0 {
 			g.logger.Errorln(gotext.Get("No AUR package found for"), " ", target)
+			packagesNotFound++
 
 			continue
 		}
@@ -468,6 +472,10 @@ func (g *Grapher) GraphFromAUR(ctx context.Context,
 	}
 
 	g.AddDepsForPkgs(ctx, aurPkgsAdded, graph)
+
+	if packagesNotFound == len(targets) {
+        return graph, &query.ErrTargetNotFound{} 
+    }
 
 	return graph, nil
 }
