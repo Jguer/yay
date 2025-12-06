e8d6c4608b48b35cc7d3fcb1ec47a03c72eaf617
Jo Garnier <git@jguer.space>
2025-12-06 11:55:26 +0000

    launch goroutine if update is needed

--- pkg/sync/sync.go
+++ pkg/sync/sync.go
@@ -63,16 +63,15 @@ func (o *OperationService) Run(ctx context.Context, run *runtime.Runtime,
 		installer.AddPostInstallHook(cleanAURDirsFunc)
 	}
 
-	go func() {
-		if !completion.NeedsUpdate(o.cfg.CompletionPath, o.cfg.CompletionInterval, false) {
-			return
-		}
-		errComp := completion.UpdateCache(ctx, run.HTTPClient, o.dbExecutor,
-			o.cfg.AURURL, o.cfg.CompletionPath, o.logger)
-		if errComp != nil {
-			o.logger.Warnln(errComp)
-		}
-	}()
+	if completion.NeedsUpdate(o.cfg.CompletionPath, o.cfg.CompletionInterval, false) {
+		go func() {
+			errComp := completion.UpdateCache(ctx, run.HTTPClient, o.dbExecutor,
+				o.cfg.AURURL, o.cfg.CompletionPath, o.logger)
+			if errComp != nil {
+				o.logger.Warnln(errComp)
+			}
+		}()
+	}
 
 	srcInfo, errInstall := srcinfo.NewService(o.dbExecutor, o.cfg,
 		o.logger.Child("srcinfo"), run.CmdBuilder, run.VCSStore, pkgBuildDirs)
